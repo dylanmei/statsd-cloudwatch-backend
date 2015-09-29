92afc637f2779163564ec8ee16cd681376dd5d49
AvengerPenguin <ross.fenning@gmail.com>
2015-09-29 12:51:46 +0100

    Refactored tests to allow for capturing more than one call to PutMetricData at once.

--- test/fake.js
+++ test/fake.js
@@ -2,14 +2,15 @@ var _ = require('underscore')
 
 var Fake = module.exports = {
   CloudWatch: function() {
-    this.Namespace = ''
-    this.MetricData = []
+    //this.Namespace = ''
+    //this.MetricData = []
+    this.params = []
   },
 }
 
 _.extend(Fake.CloudWatch.prototype, {
   putMetricData: function(params, complete) {
-    _.extend(this, params)
+    this.params.push(params);
     complete(null, {})
   }
 })
--- test/fixture.js
+++ test/fixture.js
@@ -14,5 +14,28 @@ module.exports = {
   gauges: {
     'api.num_sessions': 50,
     'statsd.timestamp_lag': 0
-  }
+  },
+  manyCounters: {
+    'counter00': 100,
+    'counter01': 100,
+    'counter02': 100,
+    'counter03': 100,
+    'counter04': 100,
+    'counter05': 100,
+    'counter06': 100,
+    'counter07': 100,
+    'counter08': 100,
+    'counter09': 100,
+    'counter10': 100,
+    'counter11': 100,
+    'counter12': 100,
+    'counter13': 100,
+    'counter14': 100,
+    'counter15': 100,
+    'counter16': 100,
+    'counter17': 100,
+    'counter18': 100,
+    'counter19': 100,
+    'counter20': 100
+  },
 }
